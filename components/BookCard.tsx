36ae3ada1944b8309ec7b71a9a8a81b35ec26ed1
50ur48h <sourabhkumrawat02@gmail.com>
2025-01-17 09:07:11 +0530

    Added Book Card and book cover.

---
+++ components/BookCard.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import BookCover from './BookCover';
+import { cn } from '@/lib/utils';
+import { Button } from './ui/button';
+
+const BookCard = (
+    {
+        id, 
+        title, 
+        genre, 
+        coverColor, 
+        coverUrl, 
+        isLoanedBook = false
+    }: Book) => <li className={cn(isLoanedBook && 'xs:w-52 w-full')}>
+        <Link href={`/books/${id}`} className={cn(isLoanedBook && 'w-full flex flex-col items-center')}>
+            <BookCover coverColor={coverColor} coverImage={coverUrl} />
+            <div className={cn('mt-4', !isLoanedBook && 'xs:max-w-40 max-w-28')}>
+                <p className='book-title'>{title}</p>
+                <p className='book-genre'>{genre}</p>
+            </div>
+
+            {isLoanedBook && (
+                <div className='mt-3 w-full'>
+                    <div className='book-loaned'>
+                        <Image src='/icons/calendar.svg' width={18} height={18} alt='calendar' className='object-contain' />
+                        <p className='text-light-100'>11 Days Left to return</p>
+                    </div>
+                    <Button className='book-overview_btn'>Download Reciept</Button>
+                </div>
+            )}
+        </Link>
+    </li>;
+
+export default BookCard;
