b03338c9b2f8e923209d327bbcf03acdb696aa6b
50ur48h <sourabhkumrawat02@gmail.com>
2025-01-18 11:14:19 +0530

    ImageKit setup with Image Upload feature.

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,28 +1,68 @@
-import React, { useState } from 'react';
+'use client';
 
-const ImageUpload = () => {
-    const [image, setImage] = useState<File | null>(null);
+import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
+import config from "@/lib/config";
+import ImageKit from "imagekit";
+import { useRef, useState } from "react";
+import Image from "next/image";
+import { toast } from "@/hooks/use-toast"
 
-    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files && e.target.files.length > 0) {
-            setImage(e.target.files[0]);
+
+const { env: { imagekit: { publicKey, urlEndpoint } } } = config;
+
+const authenticator = async () => {
+    try {
+        const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+        if(!response.ok) {
+            const errorText = await response.text();
+            throw new Error(`Request Failed with Status: ${response.status}: ${errorText}`);
         }
-    };
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        if (image) {
-            // Handle image upload logic here
-            console.log(image);
-        }
-    };
+        const data = await response.json();
+        const {signature, expire, token} = data;
+        return {signature, expire, token};
+
+    } catch (error) {
+        throw new Error("Authentication Failed: ${error.message}");
+    }
+}
+
+const ImageUpload = ({onFileChange}: { onFileChange : (filePath: string) => void}) => {
+    const ikUploadRef = useRef(null);
+    const [file, setFile] = useState<{filePath: string} | null>(null);
+    const onError = (error: any) => {
+        toast({
+            title: "Image upload failed",
+            description: `Your image can not be processed. ${console.log(error)}`,
+            variant: "destructive",
+          })
+    }
+    const onSuccess =(res: any) => {
+        setFile(res);
+        onFileChange(res.filePath);
+        toast({
+            title: "Image Uploaded",
+            description: `${res.filePath}`,
+        })
+    }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="file" accept="image/*" onChange={handleImageChange} />
-            <button type="submit">Upload Image</button>
-        </form>
+        <ImageKitProvider publicKey={publicKey} urlEndpoint={urlEndpoint} authenticator={authenticator}>
+            <IKUpload className="hidden" ref={ikUploadRef} onError={onError} onSuccess={onSuccess} fileName="test-upload.png"/>
+            <button className="upload-btn" onClick={(e) => {
+                e.preventDefault();
+                if(ikUploadRef.current){
+                    // @ts-ignore
+                    ikUploadRef.current?.click();
+                }
+            }}>
+                <Image src="/icons/upload.svg" width={20} height={20} alt="Upload Icon" className="object-contain"/>
+                <p className="text-base, text-light-100">Upload ID Card</p>
+                {file && <p className="upload-filename">{file.filePath}</p>}
+            </button>
+            {file && <IKImage path={file.filePath} alt={file.filePath} width={300} height={200} />}
+        </ImageKitProvider>
     );
 };
 
-export default ImageUpload;
+export default ImageUpload; 
