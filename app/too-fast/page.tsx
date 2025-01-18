fff31b833fb5b4205bc03d426db484d08520a2ab
50ur48h <sourabhkumrawat02@gmail.com>
2025-01-18 18:39:29 +0530

    Added Ratelimit to login/signup.

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <main className='root-container flex min-h-screen flex-col items-center justify-center'>
+            <h1 className='font-bebas-neue text-5xl font-bold text-light-100'>Woah, Slow down there speedy.</h1>
+            <p className='mt-3 text-center max-w-xl text-light-100'>Are yaar thodi der ruk jao, aache se yaad karlo ki kya id/password hai. Baar baar try karke website slow mat karo.</p>
+        </main>
+    );
+};
+
+export default Page;
